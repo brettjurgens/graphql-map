9f6051a43b16f79fc80334197b41b35cf002c6e7
Brett Jurgens <brett.jurgens@avant.com>
2018-05-23 16:18:26 -0500

    fix deprecated scalar coerce

--- lib/graphql/map.rb
+++ lib/graphql/map.rb
@@ -5,7 +5,7 @@ require "graphql/map/version"
 GraphQL::MapType = GraphQL::ScalarType.define do
   name "Map"
 
-  coerce -> (value) do
+  coerce -> (value, _ctx) do
     value.respond_to?(:to_h) ? value.to_h : nil
   end
 end
--- lib/graphql/map/version.rb
+++ lib/graphql/map/version.rb
@@ -1,5 +1,5 @@
 module Graphql
   module Map
-    VERSION = "0.2.0"
+    VERSION = "0.3.0"
   end
 end
